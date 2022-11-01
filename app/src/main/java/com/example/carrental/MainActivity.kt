c6411f2f449c7ec8603ac91eaf6bd7c29c7b9ef4
Sanzhar Altynbaev <200103012@stu.sdu.edu.kz>
2022-11-01 14:54:28 +0600

    skydka works

--- app/src/main/java/com/example/carrental/MainActivity.kt
+++ app/src/main/java/com/example/carrental/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     class Dice(val numSides: Int) {
 
         fun roll(): Int {
-            return (1..numSides).random()
+            return (1..100).random()
         }
     }
 
@@ -29,21 +31,25 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll the dice
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
 
-        // Find the ImageView in the layout
+        val dice = Dice(8)
+        val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView2)
+        val resultTextView: TextView = findViewById(R.id.textView6)
+        resultTextView.text = diceRoll.toString()
+
+
 
         // Determine which drawable resource ID to use based on the dice roll
         val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.random_1
+            2 -> R.drawable.random_2
+            3 -> R.drawable.random_3
+            4 -> R.drawable.random_4
+            5 -> R.drawable.random_5
+            6 -> R.drawable.random_6
+            7 -> R.drawable.random_7
+            else -> R.drawable.random_8
         }
 
         // Update the ImageView with the correct drawable resource ID
@@ -52,4 +58,7 @@ class MainActivity : AppCompatActivity() {
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
     }
+
+
 }
+
