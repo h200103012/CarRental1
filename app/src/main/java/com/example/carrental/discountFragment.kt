fc709e92d903b5d3908fd097543ebca066fda9bd
Yernur <200103165@stu.sdu.edu.kz>
2022-11-02 15:32:21 +0600

    All fragments

--- app/src/main/java/com/example/carrental/discountFragment.kt
+++ app/src/main/java/com/example/carrental/discountFragment.kt
@@ -1,6 +1,15 @@
 package com.example.carrental
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class discountFragment: Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.skydka, container, false)
+    }
 }
--- app/src/main/java/com/example/carrental/faqFragment.kt
+++ app/src/main/java/com/example/carrental/faqFragment.kt
@@ -1,6 +1,14 @@
 package com.example.carrental
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class faqFragment: Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.help, container, false)
+    }
 }
