6b240d510c9af0a5d0c3c2672aa5e16fadb1d131
Sanzhar Altynbaev <200103012@stu.sdu.edu.kz>
2022-11-06 17:28:49 +0600

    1

--- app/src/main/java/com/example/carrental/MainActivity.kt
+++ app/src/main/java/com/example/carrental/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.drawerlayout.widget.DrawerLayout
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.skydka)
