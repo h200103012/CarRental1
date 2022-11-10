fa48b2dee2d3509d315a636b46b8f19cad483af7
Sanzhar Altynbaev <200103012@stu.sdu.edu.kz>
2022-11-10 19:57:22 +0600

    bottom_navbar

--- app/src/main/java/com/example/carrental/MainActivity.kt
+++ app/src/main/java/com/example/carrental/MainActivity.kt
@@ -2,20 +2,50 @@ package com.example.carrental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-
-
+import android.provider.ContactsContract
+import androidx.fragment.app.Fragment
+import com.example.carrental.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.Calatog -> replaceFragment(catalogFragment())
+                R.id.Help -> replaceFragment(faqFragment())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
-
--- app/src/main/java/com/example/carrental/discountFragment.kt
+++ app/src/main/java/com/example/carrental/discountFragment.kt
@@ -6,11 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.drawerlayout.widget.DrawerLayout
+
 
 class discountFragment: Fragment(){
 
@@ -18,43 +14,4 @@ class discountFragment: Fragment(){
     ): View? {
         return inflater.inflate(R.layout.skydka, container, false)
     }
-}
-
-class Activity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.skydka)
-
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
-        rollDice()
-    }
-
-
-    class Dice(val numSides: Int) {
-
-        fun roll(): Int {
-            return (10..20).random()
-        }
-    }
-
-
-    private fun rollDice() {
-
-        val dice = Dice(8)
-        val diceRoll = dice.roll()
-        val diceImage: ImageView = findViewById(R.id.imageView2)
-        val resultTextView: TextView = findViewById(R.id.textView6)
-        resultTextView.text = diceRoll.toString()
-
-
-
-        val drawableResource = R.drawable.random_1
-
-        diceImage.setImageResource(drawableResource)
-
-        diceImage.contentDescription = diceRoll.toString()
-    }
-
 }
