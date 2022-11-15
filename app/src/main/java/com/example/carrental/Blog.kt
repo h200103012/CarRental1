e3f6abf57dff16c712a849911fad464614477e68
Yernur <200103165@stu.sdu.edu.kz>
2022-11-15 21:26:17 +0600

    MVVM in FAvorite

---
+++ app/src/main/java/com/example/carrental/Blog.kt
@@ -0,0 +1,5 @@
+package com.example.carrental
+
+data class Blog(var title:String)
+
+
--- app/src/main/java/com/example/carrental/favoriteFragment.kt
+++ app/src/main/java/com/example/carrental/favoriteFragment.kt
@@ -1,14 +1,60 @@
 package com.example.carrental
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class favoriteFragment : Fragment(){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.favorite, container, false)
+
+class favoriteFragment : AppCompatActivity() {
+    private var viewManager = LinearLayoutManager(this)
+    private lateinit var viewModel: MainViewModel
+    private lateinit var mainrecycler: RecyclerView
+    private lateinit var but: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.favorite)
+        mainrecycler = findViewById(R.id.recycler)
+        val application = requireNotNull(this).application
+        val factory = MainViewModelFactory()
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        but = findViewById(R.id.button)
+        but.setOnClickListener {
+            addData()
+        }
+
+        initialiseAdapter()
+    }
+    private fun initialiseAdapter(){
+        mainrecycler.layoutManager = viewManager
+        observeData()
+    }
+
+    fun observeData(){
+        viewModel.lst.observe(this, Observer{
+            Log.i("data",it.toString())
+            mainrecycler.adapter= NoteRecyclerAdapter(viewModel, it, this)
+        })
+    }
+
+
+    fun addData(){
+        var txtplce = findViewById<EditText>(R.id.favoriteMark)
+        var title=txtplce.text.toString()
+        if(title.isNullOrBlank()){
+            Toast.makeText(this,"Enter value!", Toast.LENGTH_LONG).show()
+        }else{
+            var blog= Blog(title)
+            viewModel.add(blog)
+            txtplce.text.clear()
+            mainrecycler.adapter?.notifyDataSetChanged()
+        }
+
     }
 }
